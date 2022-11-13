1272e102a9fd715946495ce1354f7b19a24765d4
JaacielBriseo <jaacielbris23@gmail.com>
2022-11-13 13:57:10 -0700

    React redux/ redux-toolkit config

---
+++ src/store/store.js
@@ -0,0 +1,10 @@
+import { configureStore } from "@reduxjs/toolkit";
+import { portfolioSlice } from "./portfolio/portfolioSlice";
+
+
+
+export const store = configureStore({
+    reducer:{
+        portfolio : portfolioSlice.reducer,
+    }
+})
