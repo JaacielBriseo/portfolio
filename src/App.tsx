fdb808cfde4ba6ed4dc37589c38b7f9a9d34a06b
JaacielBriseo <jaacielbris23@gmail.com>
2023-02-02 18:36:02 -0700

    Rendering routes in App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,12 @@
+import { ContactPage, HomePage, ProjectsPage } from './pages';
+import { Route } from './router';
+
 export const App = () => {
-	return <div>App</div>;
+	return (
+		<div>
+			<Route path='/' children={<HomePage />} />
+			<Route path='/contact' children={<ContactPage />} />
+			<Route path='/projects' children={<ProjectsPage />} />
+		</div>
+	);
 };
--- src/router/index.ts
+++ src/router/index.ts
@@ -1,1 +1,2 @@
 export * from './context'
+export { Route } from './components/Route'
