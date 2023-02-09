ec7fbecd03fe7509805ecf4fea3f36c690a1435f
JaacielBriseo <jaacielbris23@gmail.com>
2023-02-09 00:01:57 -0700

    Fix bug on AnimatedText and some changes on the animations
    Characters were lagging on mobile devices / characters now come from up and down

--- src/components/AnimatedText/AnimatedText.tsx
+++ src/components/AnimatedText/AnimatedText.tsx
@@ -8,7 +8,16 @@ export const AnimatedText: React.FC<Props> = ({ text }) => {
 	const words = text.split(' ');
 	useLayoutEffect(() => {
 		const ctx = gsap.context(() => {
-			gsap.to('.char', { y: 0, opacity: 1, stagger: 0.1, delay: 0.5, duration: 1, ease: 'none', repeat: 0 });
+			gsap.fromTo(
+				'.charUp',
+				{ y: -1000, opacity: 0 },
+				{ y: 0, opacity: 1, stagger: 0.1, delay: 0.2, duration: 2, ease: 'bounce', repeat: 0 }
+			);
+			gsap.fromTo(
+				'.charDown',
+				{ y: 1000, opacity: 0 },
+				{ y: 0, opacity: 1, stagger: 0.1, delay: 0.2, duration: 2, ease: 'bounce', repeat: 0 }
+			);
 		}, textRef);
 
 		return () => ctx.revert();
@@ -19,7 +28,7 @@ export const AnimatedText: React.FC<Props> = ({ text }) => {
 			{words.map(word => (
 				<div key={word}>
 					{word.split('').map((char, i) => (
-						<span key={i} className='char inline-flex translate-y-96 duration-100 opacity-0'>
+						<span key={i} className={`${i % 2 === 0 ? 'charUp' : 'charDown'} inline-flex`}>
 							{char}
 						</span>
 					))}
--- src/components/Wrapper/Wrapper.tsx
+++ src/components/Wrapper/Wrapper.tsx
@@ -5,7 +5,6 @@ type Props = {
 	children: ReactNode | ReactNode[];
 };
 export const Wrapper: React.FC<Props> = ({ children }) => {
-	const animatedBgRef = useRef<HTMLDivElement>(null);
 	const borderEffect = useRef<HTMLDivElement>(null);
 
 	useLayoutEffect(() => {
@@ -24,7 +23,7 @@ export const Wrapper: React.FC<Props> = ({ children }) => {
 						'conic-gradient(from 1turn, #213,#112 50%,#213),conic-gradient(from 1turn, transparent 20%, #08f,#f03',
 				}
 			);
-		});
+		}, borderEffect);
 		return () => ctx.revert();
 	}, []);
 
@@ -39,7 +38,7 @@ export const Wrapper: React.FC<Props> = ({ children }) => {
 			}}
 			ref={borderEffect}
 			className='bg-black bg-opacity-80 dark:bg-opacity-50 h-1/3 -mt-20 p-2 relative overflow-hidden lg:h-3/4 lg:w-8/12 lg:mx-auto lg:mt-0 lg:col-span-2 lg:row-span-3'>
-			<div ref={animatedBgRef} className='absolute inset-0 bg-cover w-full h-full scale-150' />
+			<div className='absolute inset-0 bg-cover w-full h-full scale-150' />
 			{children}
 		</div>
 	);
